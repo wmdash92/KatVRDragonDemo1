dde262385ad6a80ef660b2e2123a5a78311909a0
wmdash92 <wmdash92@gmail.com>
2021-08-27 16:08:39 +0900

    KAT VR DRAGON TEST

---
+++ Assets/02.Scripts/MoveDestination.cs
@@ -0,0 +1,15 @@
+// MoveDestination.cs
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveDestination : MonoBehaviour
+{
+
+    public Transform goal;
+
+    void Update()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
